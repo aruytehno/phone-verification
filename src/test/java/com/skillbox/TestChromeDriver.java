b01c4023e4f01a305978480d38e315abc514860d
aruytehno <aruytehno@gmail.com>
2022-07-16 21:37:39 +0300

    Stable parse

--- src/test/java/com/skillbox/TestChromeDriver.java
+++ src/test/java/com/skillbox/TestChromeDriver.java
@@ -1,5 +1,6 @@
 package com.skillbox;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,9 +16,13 @@ public class TestChromeDriver {
         options.addArguments("--disable-blink-features=AutomationControlled");
         System.setProperty("webdriver.chrome.driver", "/Users/aruytehno/IdeaProjects/phone-verification/src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.get("http://www.beeline.ru/");
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+
+//        System.out.println(driver.findElement(By.className("zodJeH")).getTagName());
+        driver.findElement(By.className("zodJeH")).click();
         System.out.println(driver.getPageSource());
+        driver.close();
     }
 }
