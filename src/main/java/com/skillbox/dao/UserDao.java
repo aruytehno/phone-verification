dc5660ca4e054b4e2b8513d31cff6bc629f600a5
aruytehno <aruytehno@gmail.com>
2022-07-16 19:50:14 +0300

    Update

---
+++ src/main/java/com/skillbox/dao/UserDao.java
@@ -0,0 +1,49 @@
+package com.skillbox.dao;
+
+
+import com.skillbox.models.Auto;
+import com.skillbox.models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.skillbox.utils.HibernateSessionFactoryUtil;
+import java.util.List;
+
+public class UserDao {
+
+    public User findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public Auto findAutoById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
+    }
+
+    public List<User> findAll() {
+        List<User> users = (List<User>)  HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("From User").list();
+        return users;
+    }
+}
